401523df2f8824cee027128ba8a9d102634588c1
Vip3rousmango <alexander@virtuallycreative.ca>
2018-05-12 00:08:54 -0400

    Added AODA-friendly Helper CSS Variables Added Print CSS Variables Added Simple Media Query Targets Updated pkg.json for SET NODE_ENV

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 let plugins = [
   require("postcss-import"),
-  require("autoprefixer")({ browsers: "last 2 versions" })
+  require("autoprefixer")({ browsers: "last 2 versions" }),
+  require('cssnano')({ preset: 'default' })
 ];
 
 if (process.env.NODE_ENV == 'production') plugins.push(require("postcss-csso"))
