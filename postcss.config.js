7a030d4fb1674a52626dcba5f86f57eadf6bf6c3
Vip3rousmango <alexander@virtuallycreative.ca>
2019-02-05 11:54:40 -0500

    Updated PostCSS Config & TravisCI test cmd

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,23 @@
 const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
-  plugins: [
-      require('postcss-csso'),
-      require('postcss-import'),
-      postcssPresetEnv({ browsers: 'last 2 versions' }),
-      require('cssnano')({
-        preset: ['default', {
-            discardComments: {
-                removeAll: true,
-            },
-        }]
-    }),
-  ],
+    plugins: [
+        require("postcss-reporter")({
+            clearReportedMessages: true,
+        }),
+        require('postcss-csso'),
+        require('postcss-import'),
+        postcssPresetEnv({
+            browsers: 'last 2 versions'
+        }),
+        require("stylelint")({
+            /* your options */ }),
+        require('cssnano')({
+            preset: ['default', {
+                discardComments: {
+                    removeAll: true,
+                },
+            }]
+        })
+    ],
 };
